82da2d29f0f8575052b88a512e60d6accbe624a9
Giddius <GordonGrossmann@gmx.at>
2021-11-13 01:06:28 +0100

    version

--- gidapptools/__init__.py
+++ gidapptools/__init__.py
@@ -5,7 +5,7 @@ WiP
 from gidapptools.meta_data.interface import setup_meta_data, get_meta_config, get_meta_info, get_meta_item, get_meta_paths
 
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 
 from pathlib import Path
